02e2986365cb60a8e88b9a469141f603fcc655c5
vbansal18 <vaibhavbansal369@gmail.com>
2023-03-21 00:32:26 +0530

    .

--- chkfke-master/AuthInMern-Authentication-In-MERN/client/src/components/Main/index.jsx
+++ chkfke-master/AuthInMern-Authentication-In-MERN/client/src/components/Main/index.jsx
@@ -75,14 +75,14 @@ return (
 
 <section class="text-gray-600 body-font">
  
-      <div class="p-4 lg:w-1/2 md:w-full">
+      <div class="p-4 lg:w-1/2 md:w-full" id='f-1'>
         <div class="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
           <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
 		  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-8 h-8" viewBox="0 0 24 24">
               <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
             </svg>
           </div>
-          <div class="flex-grow"> 
+          <div class="flex-grow">
             <h2 class="text-white-900 text-lg title-font font-medium mb-3">Deploy On Blockchain</h2>
 			<button class="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-white rounded text-base mt-4 md:mt-0" > <a href="https://opensea.io/asset/create"> Click Here </a> </button>
           </div>

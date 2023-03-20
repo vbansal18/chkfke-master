768eacdfe5872a59c56b8034473b341ed5dbdc69
vbansal18 <vaibhavbansal369@gmail.com>
2023-03-20 23:55:57 +0530

    updated ui

--- chkfke-master/AuthInMern-Authentication-In-MERN/client/src/components/Main/index.jsx
+++ chkfke-master/AuthInMern-Authentication-In-MERN/client/src/components/Main/index.jsx
@@ -1,4 +1,4 @@
-// import styles from "./styles.module.css";
+
 import './Style.css'
 import Table from 'rc-table';
 import Logo from "./1024px-Blue_double_ticks.svg.png"
@@ -25,22 +25,71 @@ return (
     </button>
   </div>
 </header>
-<body>
-	<div className="wrp1">
-		<div className="details">
-			<h2>Beneficiary Details:</h2>
-			<h5>Beneficicary Name: </h5>
-			<h5></h5>
-			<h5>Unique Identification Id:</h5>
-			<h5>Type Of Art: </h5>
-			<h5></h5>
-			<h5>Location</h5>
-			<h5></h5>
 
-			<h5></h5>
-		</div>
-	</div>
-</body>
+
+<section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto flex flex-wrap">
+    <div class="flex flex-wrap -m-4">
+      <div class="p-4 lg:w-1/2 md:w-full">
+        <div class="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
+          <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-8 h-8" viewBox="0 0 24 24">
+              <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+            </svg>
+          </div>
+          <div class="flex-grow">
+            <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Beneficiary Details</h2>
+			<p class="leading-relaxed text-base">Artisan Name : </p>
+            <p class="leading-relaxed text-base">Unique Identification Id : </p>
+            <p class="leading-relaxed text-base">Date Of Birth : </p>
+            <p class="leading-relaxed text-base">Place of Manufacturing : </p>
+         
+            {/* <a class="mt-3 text-indigo-500 inline-flex items-center">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </a> */}
+          </div>
+        </div>
+      </div>
+      <div class="p-4 lg:w-1/2 md:w-full">
+        <div class="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
+          <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
+              <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+              <circle cx="12" cy="7" r="4"></circle>
+            </svg>
+          </div>
+          <div class="flex-grow">
+            <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Type Of Art : </h2>
+            <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="text-gray-600 body-font">
+ 
+      <div class="p-4 lg:w-1/2 md:w-full">
+        <div class="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
+          <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
+              <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+              <circle cx="12" cy="7" r="4"></circle>
+            </svg>
+          </div>
+          <div class="flex-grow">
+            <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Deploy On Blockchain</h2>
+			<button class="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0" > <a href=""></a>  Click Here</button>
+          </div>
+        </div>
+      </div>
+    
+</section>
 
 
 	</>
